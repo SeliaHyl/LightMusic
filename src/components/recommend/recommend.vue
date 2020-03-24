b5ed58aadd452ce63379823fd1520a79392b31a7
SeliaHyl <2049162443@qq.com>
2020-03-24 18:55:42 +0800

    first update

---
+++ src/components/recommend/recommend.vue
@@ -0,0 +1,27 @@
+<template>
+	<div>
+		推荐页面
+	</div>
+</template>
+<script>
+	import {getRecommend} from 'api/recommend'
+	import {ERR_OK} from 'api/config'
+	export default {
+		name: 'Recommend',
+		created() {
+			this._getRecommend()
+		},
+		methods: {
+			_getRecommend() {
+				getRecommend().then((res) => {
+					if (res.code === ERR_OK) {
+						console.log(res.data.slider)
+					}
+				})
+			}
+		}
+	}
+</script>
+<style lang="stylus" scoped>
+	
+</style>
